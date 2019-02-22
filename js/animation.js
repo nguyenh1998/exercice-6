86ea3fc3634e99324296cd45c12510e3deeae009
Nguyen GitHub <>
2019-02-22 16:45:26 -0500

    Implementation animation 2 sur le sous-titre de la page

--- js/animation.js
+++ js/animation.js
@@ -2,5 +2,11 @@ window.addEventListener('load', function() {
     console.log('AAAAAAAAAAAAAAA')
     
     var titre = document.getElementsByClassName('site-title')[0]
+    var sousTitre = document.getElementsByClassName('site-description')[0]
+
     titre.classList.add('fadeIn')
+
+    titre.addEventListener('animationend', () => {
+        sousTitre.classList.add('swipeDown')
+    })
 })
