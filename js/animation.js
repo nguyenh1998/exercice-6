717fdf4ac48bf27209346b4ce652c8d5d3647e42
Nguyen GitHub <>
2019-02-22 16:32:36 -0500

    Implementation animation sur le titre de la page

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', function() {
     console.log('AAAAAAAAAAAAAAA')
     
+    var titre = document.getElementsByClassName('site-title')[0]
+    titre.classList.add('fadeIn')
 })
