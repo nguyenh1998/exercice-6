58aeb20e692498fa211ae9fca65169eb582b9e5b
Nguyen GitHub <>
2019-02-22 17:00:28 -0500

    Implementation animation 3 sur le contenu de la page

--- js/animation.js
+++ js/animation.js
@@ -3,10 +3,16 @@ window.addEventListener('load', function() {
     
     var titre = document.getElementsByClassName('site-title')[0]
     var sousTitre = document.getElementsByClassName('site-description')[0]
+    var content = document.getElementsByClassName('site-content')[0]
 
     titre.classList.add('fadeIn')
 
     titre.addEventListener('animationend', () => {
         sousTitre.classList.add('swipeDown')
+    })
+
+    sousTitre.addEventListener('animationend', () => {
+        sousTitre.style.position = 'relative'
+        content.classList.add('appearIn')
     })
 })
